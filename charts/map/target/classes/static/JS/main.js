b91cc1eb4cb7398ac184f0df66d048faac155eef
AmberYuuu <amberyoui@163.com>
2019-05-14 20:02:01 +0800

    tujidenarbar

---
+++ charts/map/target/classes/static/JS/main.js
@@ -0,0 +1,5 @@
+$(function () {
+    console.log($($('#person_intro')).offset().top - 70);
+    console.log($($('#honer')).offset().top - 70);
+    console.log($($('#academic')).offset().top - 70);
+    console.log($($('#education')).offset().top - 70);
