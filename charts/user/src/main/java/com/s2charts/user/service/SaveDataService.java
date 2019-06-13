3f29728fa196b743a0cb3723da2ba52e20e263c6
lsy361409183 <361409183@qq.com>
2019-06-13 16:30:23 +0800

    数据库取参数编辑的数据1

--- charts/user/src/main/java/com/s2charts/user/service/SaveDataService.java
+++ charts/user/src/main/java/com/s2charts/user/service/SaveDataService.java
@@ -5,11 +5,16 @@ import com.s2charts.dao.mapper.user.SysPicMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class SaveDataService {
     @Autowired
     private SysPicMapper sysPicMapper;
     public SysPic insertUserPic(int userId, String userPic, String picOption){
         return sysPicMapper.insertUserPic(userId,userPic,picOption);
+    }
+    public List<String> getPicOption(int userId){
+        return sysPicMapper.getPicOption(userId);
     }
 }
