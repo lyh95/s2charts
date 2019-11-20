3ce27b97d917b00b540a1c84d837fbc8eea1d1e5
KhanMxp <khan_mxp@163.com>
2019-11-20 20:26:47 +0800

    yu

--- charts/web/src/main/webapp/large-screen/peoplenumber/JS/index.js
+++ charts/web/src/main/webapp/large-screen/peoplenumber/JS/index.js
@@ -80,7 +80,7 @@ var hot=new Handsontable(container,{
         $("#screen-effect1").mouseenter(function () {
             //判断编辑框在不在，不在加id
          if ($('#control-panel').css("display") == 'none') {
-             $('#screen-edit1').append(" <button type='button' id='edit-button' value='编辑' style='z-index: 2;'>编辑</button>")
+             $('#screen-edit1').append(" <button type='button' id='edit-button' value='编辑'>编辑</button>")
              //点编辑时，出现编辑框
              $("#screen-edit1 button").click(function () {
                  $("#control-panel").fadeIn()
@@ -110,7 +110,7 @@ var hot=new Handsontable(container,{
                 //判断编辑框在不在，不在加id
                 if ($('#control-panel').css("display") == 'none') {
 
-                    $('#screen-edit2').append(" <button type='button' id='edit-button' value='编辑' style='z-index: 2;'>编辑</button>")
+                    $('#screen-edit2').append(" <button type='button' id='edit-button' value='编辑'>编辑</button>")
                     //点编辑时，出现编辑框
                     $("#screen-edit2 button").click(function () {
                         $("#control-panel").fadeIn()
@@ -134,7 +134,7 @@ var hot=new Handsontable(container,{
         $("#screen-effect3").mouseenter(function ()
             {
                 if ($('#control-panel').css("display") == 'none') {
-                    $('#screen-edit3 ').append(" <button type='button' id='edit-button' value='编辑' style='z-index: 2;'>编辑</button>")
+                    $('#screen-edit3 ').append(" <button type='button' id='edit-button' value='编辑'>编辑</button>")
                     //点编辑时，出现编辑框
                     $("#screen-edit3 button").click(function () {
                         $("#control-panel").fadeIn()
@@ -164,7 +164,7 @@ var hot=new Handsontable(container,{
         //第四张图
         $("#screen-effect4").mouseenter(function () {
             if ($('#control-panel').css("display") == 'none') {
-                $('#screen-edit4').append(" <button type='button' id='edit-button' value='编辑' style='z-index: 2;'>编辑</button>")
+                $('#screen-edit4').append(" <button type='button' id='edit-button' value='编辑' >编辑</button>")
                   //点编辑时，出现编辑框
                 $("#screen-edit4 button").click(function () {
                     $("#control-panel").fadeIn();
@@ -183,8 +183,6 @@ var hot=new Handsontable(container,{
         $("#screen-effect4").mouseleave(function () {
             if ($("#control-panel").css("display") == 'none') {
                 $("#edit-button").remove();
-                // hot.loadData(null);
-                // console.log('清空数据1')
                 $("#chart-bar-color-select-1").remove();
                 $("#chart-bar-color-select-2").remove();
                 $("#chart-bar-color-select-3").remove();
@@ -195,7 +193,7 @@ var hot=new Handsontable(container,{
         $("#screen-effect5").mouseenter(function () {
             if ($('#control-panel').css("display") == 'none') {
 
-                $('#screen-edit5 ').append(" <button type='button' id='edit-button' value='编辑' style='z-index: 2;'>编辑</button>")
+                $('#screen-edit5 ').append(" <button type='button' id='edit-button' value='编辑'>编辑</button>")
                 //点编辑时，出现编辑框
                 $("#screen-edit5 button").click(function () {
                     $("#control-panel").fadeIn();
@@ -214,9 +212,6 @@ var hot=new Handsontable(container,{
         $("#screen-effect5").mouseleave(function () {
             if ($("#control-panel").css("display") == 'none') {
                 $("#edit-button").remove();
-                // hot.loadData(null);
-                // console.log('清空数据1')
-
                 $("#chart-bar-color-select-1").remove();
                 $("#chart-bar-color-select-2").remove();
             }else {}
@@ -360,7 +355,6 @@ var hot=new Handsontable(container,{
             CloseBtn()
         })
     });
-
 //引入表格
 function Render1(x) {
     var path=x,
@@ -446,5 +440,48 @@ Render3(3);
 Render4(4);
 Render5(5);
 
+//拖拽编辑框事件
+
+var dv = document.getElementById("control-panel"); //获取元素
+var x = 0;
+var y = 0;
+var l = 0;
+var t = 0;
+var isDown = false;
+//鼠标按下事件
+dv.onmousedown = function(e) {
+    //获取x坐标和y坐标
+    x = e.clientX;
+    y = e.clientY;
+
+    //获取左部和顶部的偏移量
+    l = dv.offsetLeft;
+    t = dv.offsetTop;
+    //开关打开
+    isDown = true;
+    //设置样式
+    dv.style.cursor = 'move';
+}
+//鼠标移动
+window.onmousemove = function(e) {
+    if (isDown == false) {
+        return;
+    }
+    //获取x和y
+    var nx = e.clientX;
+    var ny = e.clientY;
+    //计算移动后的左偏移量和顶部的偏移量
+    var nl = nx - (x - l);
+    var nt = ny - (y - t);
+
+    dv.style.left = nl + 'px';
+    dv.style.top = nt + 'px';
+}
+//鼠标抬起事件
+dv.onmouseup = function() {
+    //开关关闭
+    isDown = false;
+    dv.style.cursor = 'default';
+}
 // })
 
